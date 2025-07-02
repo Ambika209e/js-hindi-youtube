7d5d4dc526c87483780917a475c0fc44720170bb
Ambika209e <ambikavishnoi411@gmail.com>
2025-07-02 16:11:33 +0000

    new changes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 121
+let accountEmail = "ambika@gmail.com"
+var accountPassword = "1234"
+accountCity = "Indore"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
