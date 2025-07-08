da4a69a459919eea9c5c9a58228f72e51f5b0c03
Ambika209e <ambikavishnoi411@gmail.com>
2025-07-08 16:02:39 +0000

    while and do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+     console.log(`value of index is ${index}`);
+     index = index + 2;
+}
+
+let myArr = ["apple","banana","mango"];
+let i = 0;
+while(i<myArr.length){
+    console.log(`value is ${myArr[i]}`)
+    i++;
+}
+
+// do-while loop
+
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
